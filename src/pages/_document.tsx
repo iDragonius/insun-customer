8dde0abeb0c9cc1ae7bf5f9bd43476017c9039c5
Ruslan <rustamovruslan1205@gmail.com>
2024-03-26 10:17:54 +0400

    google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,11 +9,11 @@ export default function Document() {
           id={"google-tag"}
           strategy={"afterInteractive"}
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            __html: `((function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TPLR2SXW');`,
+})(window,document,'script','dataLayer','GTM-TMXWDFK3');`,
           }}
         />
       </Head>
@@ -21,12 +21,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-         <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TPLR2SXW"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
+         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMXWDFK3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
         `,
           }}
         ></noscript>
