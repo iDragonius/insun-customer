1379f75cfc03758d0bcba13db1ffcb9d25bde0a6
Ruslan <rustamovruslan1205@gmail.com>
2024-06-21 20:55:08 +0400

    test

--- src/components/layout/ui/header/languages/index.tsx
+++ src/components/layout/ui/header/languages/index.tsx
@@ -152,7 +152,6 @@ const Languages: FC<LanguagesProps> = () => {
     //@ts-ignore
     new window.google.translate.TranslateElement(
       {
-        pageLanguage: "auto",
         autoDisplay: false,
         includedLanguages: "ru,en,az", // If you remove it, by default all google supported language will be included
         //@ts-ignore
